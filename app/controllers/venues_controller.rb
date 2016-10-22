173427b152e406ef5c5c112246af35c3f0c29f79
Corey <c.brooks96@live.ca>
2016-10-21 18:17:51 -0700

    Venues show up on map

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,12 @@
 class VenuesController < ApplicationController
+
+  def index
+    @venues = Venue.all
+    @hash = Gmaps4rails.build_markers(@venues) do |venue, marker|
+      marker.lat venue.latitiude
+      marker.lng venue.longitude
+      marker.infowindow venue.name
+    end
+  end
+
 end
