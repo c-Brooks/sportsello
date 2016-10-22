3d1b7512dc1aa870bd73552b31eaeb4b8a594992
Anthony Morris <anthony@amorrissound.com>
2016-10-22 22:40:48 +0000

    Confirm NHL data is scraping properly in the scheduler

--- app/crawlers/scheduler.rb
+++ app/crawlers/scheduler.rb
@@ -6,4 +6,9 @@ class Scheduler
   nhl_games = NhlCrawler.new(tomorrow).crawl
   nba_games = NbaCrawler.new(tomorrow).crawl
   mlb_games = MlbCrawler.new(tomorrow).crawl
+
+  # Put the data into the database
+  nhl_games.each do |game|
+    puts game
+  end
 end
