41cf412136a6559ecd1b3846338906d61ccdc4f6
Corey <c.brooks96@live.ca>
2016-10-21 18:21:44 -0700

    Minor refactoring

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,4 @@
 class Event < ActiveRecord::Base
-
-
   has_many :can_hosts
   has_many :venues, through: :can_hosts
   belongs_to :sport
@@ -10,14 +8,13 @@ class Event < ActiveRecord::Base
   validates :team1_id, numericality: { only_integer: true }
   validates :team2_id, numericality: { only_integer: true }
 
-
 private
 
-def event_datetime_is_date?
-   if !event_datetime.is_a?(DateTime)
-    puts(:event_datetime)
-     errors.add(:event_datetime, 'must be a valid date')
-   end
-end
+  def event_datetime_is_date?
+     if !event_datetime.is_a?(DateTime)
+      puts(:event_datetime)
+       errors.add(:event_datetime, 'must be a valid date')
+     end
+  end
 
 end
