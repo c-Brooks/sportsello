ae1edbbd589b43130c9ecc1b71c6f1ba705adaf1
Anthony Morris <anthony@amorrissound.com>
2016-10-22 01:03:25 +0000

    Fix issue with date not working

--- app/crawlers/nhl_crawler.rb
+++ app/crawlers/nhl_crawler.rb
@@ -5,12 +5,17 @@ class NhlCrawler
     @date = date
   end
 
-  base_url "https://ca.sports.yahoo.com/"
-  path "/nhl/scoreboard/?date=#{@date}"
-
-  games "css=.game", :iterator do
-    date "css=.time"
-    team1 "css=.away .team"
-    team2 "css=.home"
+  def crawl
+    get_this_path = "/nhl/scoreboard/?date=#{@date}"
+    Wombat.crawl do
+      base_url "https://ca.sports.yahoo.com/"
+      path get_this_path
+      games "css=.game", :iterator do
+        date "css=.time"
+        team1 "css=.away .team"
+        team2 "css=.home"
+      end
+    end
   end
+
 end
