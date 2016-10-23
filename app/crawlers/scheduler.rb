8a597fc8c71a592604932b8d2ed89398d35750b9
Anthony Morris <anthony@amorrissound.com>
2016-10-23 21:40:23 +0000

    Go through each set of results and add games to the database

--- app/crawlers/scheduler.rb
+++ app/crawlers/scheduler.rb
@@ -1,22 +1,44 @@
 class Scheduler
-  # Start crawling games a day in advance
-  tomorrow = Date.tomorrow
+  def initialize
+    # Start crawling games a day in advance
+    @tomorrow = Date.tomorrow
 
-  # Crawl the all the sports
-  nhl_games = NhlCrawler.new(tomorrow).crawl
-  nba_games = NbaCrawler.new(tomorrow).crawl
-  mlb_games = MlbCrawler.new(tomorrow).crawl
+    # Crawl the all the sports
+    @nhl_games = NhlCrawler.new(@tomorrow).crawl
+    @nba_games = NbaCrawler.new(@tomorrow).crawl
+    @mlb_games = MlbCrawler.new(@tomorrow).crawl
 
-  # Put the data into the database
-  nhl_games['games'].each do |game|
-    sport = Sport.find_by_name 'NHL'
+    # Go through each group of games
+    @nhl_games['games'].each do |game|
+      @sport = Sport.find_by_name 'NHL'
+      add_game(game, @sport)
+    end
+
+    @nba_games['games'].each do |game|
+      @sport = Sport.find_by_name 'NBA'
+      add_game(game, @sport)
+    end
+
+    @mlb_games['games'].each do |game|
+      @sport = Sport.find_by_name 'MLB'
+      add_game(game, @sport)
+    end
   end
 
-  nba_games['games'].each do |game|
-    sport = Sport.find_by_name 'NBA'
-  end  n
+  private
 
-  mlb_games['games'].each do |game|
-    sport = Sport.find_by_name 'MLB'
+  # Add game to database
+  def add_game(game, sport)
+    # Find teams or creat them if they don't exist
+    @team1 = Team.find_or_create_by name: game['team1']
+    @team2 = Team.find_or_create_by name: game['team2']
+
+    Game.create({
+      sport_id: sport.id,
+      team1_id: @team1.id,
+      team2_id: @team2.id,
+      game_datetime: game['date']
+    })
   end
+
 end
