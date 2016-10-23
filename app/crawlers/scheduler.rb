8aff903d25413468781ffddd6a00a39be3d4c540
Anthony Morris <anthony@amorrissound.com>
2016-10-23 23:00:02 +0000

    Make sure date is being stored properly as UTC

--- app/crawlers/scheduler.rb
+++ app/crawlers/scheduler.rb
@@ -1,27 +1,34 @@
 class Scheduler
   def initialize
     # Start crawling games a day in advance
-    @tomorrow = Date.tomorrow
+    # up to 30 days
+    tomorrow = Date.tomorrow
+    future = tomorrow + 30.days
 
-    # Crawl the all the sports
-    @nhl_games = NhlCrawler.new(@tomorrow).crawl
-    @nba_games = NbaCrawler.new(@tomorrow).crawl
-    @mlb_games = MlbCrawler.new(@tomorrow).crawl
+    tomorrow.upto(future) do |date|
+      # Crawl the all the sports
+      nhl_games = NhlCrawler.new(date).crawl
+      nba_games = NbaCrawler.new(date).crawl
+      mlb_games = MlbCrawler.new(date).crawl
 
-    # Go through each group of games
-    @nhl_games['games'].each do |game|
-      @sport = Sport.find_by_name 'NHL'
-      add_game(game, @sport)
-    end
+      # Go through each group of games
+      nhl_games['games'].each do |game|
+        sport = Sport.find_by_name 'NHL'
+        game['date'] = date.to_s + ' ' + game['date']
+        add_game(game, sport)
+      end
 
-    @nba_games['games'].each do |game|
-      @sport = Sport.find_by_name 'NBA'
-      add_game(game, @sport)
-    end
+      nba_games['games'].each do |game|
+        sport = Sport.find_by_name 'NBA'
+        game['date'] = date.to_s + ' ' + game['date']
+        add_game(game, sport)
+      end
 
-    @mlb_games['games'].each do |game|
-      @sport = Sport.find_by_name 'MLB'
-      add_game(game, @sport)
+      mlb_games['games'].each do |game|
+        sport = Sport.find_by_name 'MLB'
+        game['date'] = date.to_s + ' ' + game['date']
+        add_game(game, sport)
+      end
     end
   end
 
@@ -30,13 +37,13 @@ class Scheduler
   # Add game to database
   def add_game(game, sport)
     # Find teams or creat them if they don't exist
-    @team1 = Team.find_or_create_by name: game['team1']
-    @team2 = Team.find_or_create_by name: game['team2']
+    team1 = Team.find_or_create_by name: game['team1']
+    team2 = Team.find_or_create_by name: game['team2']
 
     Game.create({
       sport_id: sport.id,
-      team1_id: @team1.id,
-      team2_id: @team2.id,
+      team1_id: team1.id,
+      team2_id: team2.id,
       game_datetime: game['date']
     })
   end
