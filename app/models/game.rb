78516f96419ad87cd421351cad5ce54d1c2815d9
Anthony Morris <anthony@amorrissound.com>
2016-10-24 20:36:21 +0000

    Added dependant destroy on events (for destroying games)

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   belongs_to :sport
-  has_one :event
+  has_one :event, dependent: :destroy
   validates :game_datetime, presence: true
   validates :team1_id, numericality: { only_integer: true }
   validates :team2_id, numericality: { only_integer: true }
