58d46e2b3565d58a4d049dd821836f01e95f0f89
Corey <c.brooks96@live.ca>
2016-10-27 16:33:55 -0700

    Reset Capistrano files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server '172.31.25.139', user: 'deploy', roles: %w{web app db}
-# server '52.2.139.74', user: 'deploy', roles: %w{web app db}
+
 
 # role-based syntax
 # ==================
