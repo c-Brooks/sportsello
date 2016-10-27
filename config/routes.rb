d92a3e6148a8dcb9c7beb70123806f9aa8f8b3e9
Corey <c.brooks96@live.ca>
2016-10-26 19:57:26 -0700

    Added method and routing for creating new user

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     get 'auth/:provider/callback', to: 'sessions#create'
     get 'auth/failure', to: redirect('/')
     get 'signout', to: 'sessions#destroy', as: 'signout'
-    
-    resources :users, only: [:new]
+
+    resources :users, only: [:new, :create]
     resources :sessions, only: [:new, :create, :destroy]
     resources :games, only: [:index]
     resources :reviews, only:[:delete]
