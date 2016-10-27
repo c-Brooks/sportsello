0fd46f4c84756023cc7db899e7e93b8e79c6fe22
Spencer Tranter <spencertranter95@gmail.com>
2016-10-27 18:29:15 +0000

    Changed decending to ascending and removed console logs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,7 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all.order('game_datetime ASC').where("game_datetime >= ?", params['game_datetime']).limit(10)
-    puts @games
-    puts params
+
     json = Jbuilder.new do |j|
       j.games @games do |game|
         j.game game.id
