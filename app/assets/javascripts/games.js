4733665bdd9c53809bb8884e41a0b912131cf023
Spencer Tranter <spencertranter95@gmail.com>
2016-10-27 18:27:35 +0000

    Fixed game query to get ascending games by date

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function() {
+  var count = 1;
 
   var games = new Vue({
     el: '#games',
@@ -7,22 +8,35 @@ $(document).ready(function() {
       games: []
     },
     created: function() {
-      console.log('Loading');
       this.scroll();
       var that = this;
-      var lastDateTimeString = getLastDateTime(this.games)
+      var lastDateTimeString = getDateTime()
+      console.log(lastDateTimeString);
       $.ajax({
         url: `/games.json?game_datetime=${lastDateTimeString}`,
         success: function(res) {
-          that.games = res.games;
+           res.games.forEach(function(game){
+              that.games.push(game);
+            });
         }
       });
     },
     methods: {
       scroll: function() {
+        var then = this;
         window.addEventListener('scroll', function () {
           if($(window).scrollTop() + $(window).height() == $(document).height()) {
-            console.log('end of page');
+            var that = then;
+            var newDateTimeString = getLastDateTime(that.games);
+            $.ajax({
+              url: `/games.json?game_datetime=${newDateTimeString}`,
+              success: function(res) {
+                console.log('doing it');
+                res.games.forEach(function(game){
+                  that.games.push(game);
+                });
+              }
+            });
           }
         })
       }
@@ -33,16 +47,14 @@ $(document).ready(function() {
     var today = new Date();
       var yr = today.getFullYear();
       var month = today.getMonth() + 1;
-      var day = today.getDate();
-      return yr + '-' + month + '-' + day + ' 00:00:00';
+      var day = today.getDate() - 1;
+      return yr + '-' + month + '-' + day + ' 00:00:00.000000';
   }
 
   function getLastDateTime(games_array) {
-    if (games_array.length === 0) {
-      return getDateTime();
-    } else {
+    console.log(JSON.stringify(games_array[games_array.length - 1]))
       return games_array[games_array.length - 1].datetime;
-    }
   }
+
 
 });
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all.order('game_datetime DESC').where("game_datetime >= ?", params['game_datetime']).limit(35)
+    @games = Game.all.order('game_datetime ASC').where("game_datetime >= ?", params['game_datetime']).limit(10)
+    puts @games
     puts params
     json = Jbuilder.new do |j|
       j.games @games do |game|
