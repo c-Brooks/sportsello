083db57254661b1ed918918ff322a7547610a013
Corey <c.brooks96@live.ca>
2016-10-28 11:42:29 -0700

    Edited user validations so they work with/without Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
-  # has_secure_password
+
+  has_secure_password
   has_many :reviews
   has_many :venues
-  # before_save :downcase_email
+  before_save :downcase_email
   EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :name, :presence => true, :length => { :in => 3..20 }
-  # validates :email, :uniqueness => true, :format => EMAIL_REGEX
-  # validates :password, :confirmation => true
-  # validates_length_of :password, :in => 6..20, :on => :create
+  validates :email, :uniqueness => true, :format => EMAIL_REGEX, if: 'email.present?'
+  validates :password, :confirmation => true, if: 'password.present?'
+  validates_length_of :password, :in => 6..20, :on => :create, if: 'password.present?'
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -18,6 +19,10 @@ class User < ActiveRecord::Base
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
+  end
+
+  def downcase_email
+    email.downcase!
   end
 
 end
