2f5c3ee82ad2871cb10af269616f92bc394ddd34
Anthony Morris <anthony@amorrissound.com>
2016-10-28 21:00:59 +0000

    Fix issues with registration for on the backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params[:user][:email].downcase!
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params[:email].downcase!
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 
 end
