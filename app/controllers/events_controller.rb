5fc9370cb9448679d6cd8ce5bf90e9bd0da31d6a
Anthony Morris <anthony@amorrissound.com>
2016-10-31 18:02:22 +0000

    Setup attending method in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,11 @@ class EventsController < ApplicationController
   end
 
   def attending
-
+    Attendee.create(attendee_params)
   end
+
+  private
+    def attendee_params
+      params.require(:attendee).permit(:user_id, :event_id)
+    end
 end
