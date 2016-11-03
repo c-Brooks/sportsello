fd86c9e6ad60fef9ac22435c31d9f83f2c5f892a
Spencer Tranter <spencertranter95@gmail.com>
2016-11-03 05:42:26 +0000

    Removed event searching to debug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
    def index
     @games = Fuzzy_Search.new(params[:query]).searchGames
     @venues = Fuzzy_Search.new(params[:query]).searchVenues
-    @events = Fuzzy_Search.new(params[:query]).searchEvents
+    # @events = Fuzzy_Search.new(params[:query]).searchEvents
 
     json = Jbuilder.new do |j|
       j.games @games do |game|
@@ -21,15 +21,15 @@ class SearchController < ApplicationController
         j.address venue.address
         j.website venue.website
       end
-      j.events @events do |event|
-        j.event event.id
-        j.name event.name
-        j.venue Venue.find(event.venue_id), :name
-        j.game Game.find(event.game_id), :id
-        j.sport Sport.find(Game.find(event.game_id).sport_id), :name
-        j.team1 Sport.find(Game.find(event.game_id).team1_id), :name
-        j.team2 Sport.find(Game.find(event.game_id).team2_id), :name
-      end
+      # j.events @events do |event|
+      #   j.event event.id
+      #   j.name event.name
+      #   j.venue Venue.find(event.venue_id), :name
+      #   j.game Game.find(event.game_id), :id
+      #   # j.sport Sport.find(Game.find(event.game_id).sport_id), :name
+      #   j.team1 Sport.find(Game.find(event.game_id).team1_id), :name
+      #   j.team2 Sport.find(Game.find(event.game_id).team2_id), :name
+      # end
     end.target!
 
     render :json => json
