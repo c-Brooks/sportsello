539e2a1a5405c6b80bd692854144154421e1ee8f
Corey <c.brooks96@live.ca>
2016-11-04 17:19:58 -0700

    Added splash page at root
    Changed root code to live at '/dev'

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
     get 'app/index'
 
-    root to: 'home#index'
+    get 'dev', to: 'home#index'
+
+    root to: 'landing#index'
 
     get 'auth/:provider/callback', to: 'sessions#create'
     get 'auth/failure', to: redirect('/')
